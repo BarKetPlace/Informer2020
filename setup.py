22535c1c8424b66a1bf29b03f6a6f6801639b5f3
Antoine Honoré <honore@kth.se>
2023-06-13 18:04:45 +0200

    renam

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@
 from distutils.core import setup
 
 setup(name='informerfork',
-      packages=['informerfork', 'informerfork/models'],
+      packages=['informerfork', 'informerfork/models', 'informerfork/utils'],
      )
